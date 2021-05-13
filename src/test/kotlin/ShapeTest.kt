cc0c599367766aaa4f1cc1a81c7c404b8dccf71d
Josep Anguera Peralta <josep.anguera@wallapop.com>
2021-05-13 15:41:58 +0200

    Doesnt beat

--- src/test/kotlin/ShapeTest.kt
+++ src/test/kotlin/ShapeTest.kt
@@ -1,6 +1,7 @@
 import Shape.Paper
 import Shape.Rock
 import Shape.Scissors
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
@@ -21,5 +22,35 @@ class ShapeTest {
     @Test
     fun `scissors beats paper`() {
         assertTrue(Scissors.beats(Paper))
+    }
+
+    @Test
+    fun `paper doesn't beat scissor`() {
+        assertFalse(Paper.beats(Scissors))
+    }
+
+    @Test
+    fun `scissor doesn't beat rock`() {
+        assertFalse(Scissors.beats(Rock))
+    }
+
+    @Test
+    fun `rock doesn't beat paper`() {
+        assertFalse(Rock.beats(Paper))
+    }
+
+    @Test
+    fun `paper doesn't beat paper`() {
+        assertFalse(Paper.beats(Paper))
+    }
+
+    @Test
+    fun `scissor doesn't beat scissor`() {
+        assertFalse(Scissors.beats(Scissors))
+    }
+
+    @Test
+    fun `rock doesn't beat rock`() {
+        assertFalse(Rock.beats(Rock))
     }
 }
