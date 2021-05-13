6271579eb1439a89be9e8e1def233d4784e9caae
Josep Anguera Peralta <josep.anguera@wallapop.com>
2021-05-13 15:58:46 +0200

    NO REGRETS

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,16 +2,4 @@ import Shape.Paper
 
 fun main() {
     println(Paper.beats(Paper))
-    /*
-    Stage 1
-    Write code to determine whether one shape object beats another. e.g. aPaper.beats(aRock)
-
-    Do this for all combinations
-
-    - rock crushes scissors
-    - paper covers rock
-    - scissors cut paper
-    */
-
-
 }
